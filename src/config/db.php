a91c543ecb1f2f6b226b11656494b1ed83434ea3
FinnSupra <jeux.leopold@gmail.com>
2026-01-27 15:38:47 +0100

    .

---
+++ src/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost:3306',
+        'dbname' => 'producthub',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4'
+    ]
+];
