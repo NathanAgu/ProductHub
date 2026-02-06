15b00cf49f4fd28383109f4ff067e2167c7063ac
Nathan <nathan.agu.2103@gmail.com>
2026-02-06 17:48:13 +0100

    feat: add category management and integrate sizes into cart functionality

--- src/config/params.php
+++ src/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'pdo_baseUrl' => '/pdo',
-    'sizes' => ['XS', 'S', 'M', 'L', 'XL'],
+    'sizes' => ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     'brands' => ['Nike', 'Adidas', 'Stüssy', 'Supreme'],
     'colors' => ['Rouge', 'Bleu', 'Vert', 'Noir', 'Blanc', 'Jaune', 'Gris', 'Rose', 'Violet', 'Orange'],
 ];
