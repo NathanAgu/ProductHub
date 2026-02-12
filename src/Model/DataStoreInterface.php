d3f14c81e4e7e7174e846d9583ad5bb974fe2498
Nathan <nathan.agu.2103@gmail.com>
2026-02-12 11:36:56 +0100

    refactor: remove unused methods from CartPdoDataStore and DataStoreInterface

--- src/Model/DataStoreInterface.php
+++ src/Model/DataStoreInterface.php
@@ -5,9 +5,6 @@ interface DataStoreInterface
 {
     public function getAll();
     public function getById(string $id);
-    // public function getAllBrands();
-    // public function getAllColors();
-    // public function searchFilters(string $name = '', array $priceRange = [], array $brands = [], array $colors = []);
     public function create(array $data);
     public function update(string $id, array $data);
     public function delete(string $id): bool;
