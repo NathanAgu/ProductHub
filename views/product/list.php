fa68f0a802c336b47ca73d3c699ba349e589482f
FinnSupra <jeux.leopold@gmail.com>
2026-03-24 14:46:29 +0100

    il reste ça aussi

--- views/product/list.php
+++ views/product/list.php
@@ -108,7 +108,7 @@ $selectedCategories = $_GET['category'] ?? [];
 <?php if (empty($products)): ?>
     <p>Aucun produit trouvé.</p>
 <?php else: ?>
-    <table>
+    <Fble>
         <thead>
         <tr>
             <th>Nom</th>
